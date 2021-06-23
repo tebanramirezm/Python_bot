6990d7e7dcfcc5d2d202ae5618db0e04c5c9f229
Jordan Esteban Ramirez Mejia <jordanrm@familia.com.co>
2021-06-23 00:25:00 -0500

    añadiendo gitignore

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 import time
 import os, time
+import csv
 
-driver = webdriver.Chrome(executable_path=r"C:\scripts\Download\chromedriver.exe")
+options = webdriver.ChromeOptions();
+options.add_argument('--user-data-dir=C:\\scripts\\Download\\User Data')
+driver = webdriver.Chrome(executable_path=r"C:\scripts\Download\chromedriver.exe", chrome_options=options)
+driver.get('https://web.whatsapp.com/')
 
-driver.get("https://web.whatsapp.com/")
 time.sleep(10)
 
-celular = "telefono"
-mensaje = "mensaje"
+with open('datos.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    for row in csv_reader:
+        celular = (f'{row[0]}')
+        mensaje = (f'{row[1]}')
 
 driver.get("https://wa.me/"+celular+"?text="+mensaje)
 time.sleep(5)
@@ -16,11 +23,13 @@ time.sleep(5)
 boton = driver.find_elements_by_xpath("//*[@id='action-button']")[0]
 boton.click()
 time.sleep(5)
-boton = driver.find_elements_by_xpath("//*[@id='fallback_block']/div/div/a")[0]
-boton.click()
-time.sleep(30)
 
-#boton enviar                        //*[@id='main']/footer/div[1]/div[3]
+try:
+    boton = driver.find_elements_by_xpath("//*[@id='fallback_block']/div/div/a")[0]
+    boton.click()
+    time.sleep(30)
+except: "Ya se encuentra registrado el whatsapp en el automatizador"
+
 boton = driver.find_elements_by_xpath("//*[@id='main']/footer/div[1]/div[3]")[0]
 boton.click()
 time.sleep(5)
