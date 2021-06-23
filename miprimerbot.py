a1ce808ff2d1c5f68daa3004be01d88717700952
Jordan Esteban Ramirez Mejia <jordanrm@familia.com.co>
2021-06-22 22:43:32 -0500

    Automatizando logueo

--- miprimerbot.py
+++ miprimerbot.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
-import time 
-import os, time 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import json
+import pickle
+import pprint
+import time
+  
+""" Este código, permite usar selenium con la sesión iniciada de Whatsapp, mejorando así 
+    el automatismo"""
 
-""" Lo primero que haremos será ejecutar el driver desde la ruta de preferencia"""
-
-driver = webdriver.Chrome(executable_path=r"C:\scripts\Download\chromedriver.exe")
-
-driver.get("http://www.python.org")
-time.sleep(10)
-
-driver.close
-
-
+options = webdriver.ChromeOptions();
+options.add_argument('--user-data-dir=C:\\scripts\\Download\\User Data')
+driver = webdriver.Chrome(executable_path=r"C:\scripts\Download\chromedriver.exe", chrome_options=options)
+driver.get('https://web.whatsapp.com/')
